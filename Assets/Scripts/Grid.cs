9464b7a62d3b9d443b49709a52491c7c0ef1c765
Nguyen Phan <nqphan212@gmail.com>
2016-01-10 19:59:15 -0500

    Add a player to the scene. Write codes to test the posotion of the player in the scene

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
+	public Transform player;
+
 	public LayerMask unwalkableMask;
 	public Vector2 gridWorldSize;
 	public float nodeRadius;
@@ -17,22 +19,46 @@ public class Grid : MonoBehaviour {
 		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
 		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
 		CreateGrid ();
+
+
 	}
 
 	void CreateGrid()
 	{
 		grid = new Node[gridSizeX,gridSizeY];
+		//the bottom left point of the grid.
 		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+		//Me
+		//Debug.Log (worldBottomLeft + "worldBottomLeft");
 
 		for(int x = 0; x < gridSizeX; x++)
 		{
 			for(int y = 0; y < gridSizeY; y++)
 			{
+				//postion of each point in the grid
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius)	;
+
+				Debug.Log(worldPoint + "worldPoint");
+
 				bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
 				grid[x,y] = new Node(walkable, worldPoint);
 			}
 		}
+	}
+
+	public Node NodeFromWorldPoint(Vector3 worldPostion)
+	{
+		float percentX = (worldPostion.x + gridWorldSize.x/2) / gridWorldSize.x;
+		float percentY = (worldPostion.z + gridWorldSize.y/2) / gridWorldSize.y;
+
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+		return grid[x, y];
 	}
 
 	void OnDrawGizmos()
@@ -40,9 +66,16 @@ public class Grid : MonoBehaviour {
 		Gizmos.DrawWireCube (transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
 		if (grid != null) {
+			Node playerNode = NodeFromWorldPoint(player.position);
+
 			foreach(Node n in grid)
 			{
 				Gizmos.color = (n.walkable)? Color.white : Color.red;
+				if(playerNode == n)
+				{
+					Gizmos.color = Color.cyan;
+				}
+
 				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
 			}
 		}
